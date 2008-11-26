6359e5b8babd635fb085de7f0f7a420c36f93dd0
John Beppu <john.beppu@gmail.com>
2008-11-25 17:49:18 -0800

    more detail in fcgi example

--- lib/Squatting/On/HTTP/Engine.pm
+++ lib/Squatting/On/HTTP/Engine.pm
@@ -69,6 +69,10 @@ Squatting::On::HTTP::Engine - run Squatting apps on top of HTTP::Engine
 Squatting on top of HTTP::Engine::Interface::ServerSimple
 
   # app_server_simple.pl
+
+  #!/usr/bin/perl
+  use strict;
+  use warnings;
   use App 'On::HTTP::Engine';
   App->init;
   App->http_engine(
@@ -82,12 +86,41 @@ Squatting on top of HTTP::Engine::Interface::ServerSimple
 Squatting on top of HTTP::Engine::Interface::FCGI
 
   # app_fastcgi.pl
+
+  #!/usr/bin/perl
+  use strict;
+  use warnings;
+  use Getopt::Long;
   use App 'On::HTTP::Engine';
+
+  # options
+  %_ = (
+    leave_umask => 0,
+    keep_stderr => 0,
+    no_intr     => 0,
+    detatch     => 0,
+    manager     => 'FCGI::ProcManager',
+    nproc       => 1,
+    pidfile     => 'app.pid',
+    listen      => '8000',
+  );
+  
+  GetOptions(
+    \%_,
+    'leave_umask',
+    'keep_stderr',
+    'no_intr',
+    'detatch',
+    'manager=s',
+    'nproc=i',
+    'pidfile=s',
+    'listen=s',
+  );
+
   App->init;
   App->http_engine(
     interface => 'FCGI',
-    args      => {
-    }
+    args      => \%_,
   )->run;
 
 Squatting on top of HTTP::Engine::Interface::ModPerl
